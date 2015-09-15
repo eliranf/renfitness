d2bf688b8694f2d599b6f873aa7089b46bc95946
eliranf77 <eliranf@gmail.com>
2015-09-15 14:05:22 +0000

    added business

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  root             'pages#home'
-  get 'help'    => 'pages#help'
-  get 'photos'  => 'pages#photos'
-  get 'about'   => 'pages#about'
-  get 'contact' => 'pages#contact'
+  root              'pages#home'
+  get 'help'     => 'pages#help'
+  get 'photos'   => 'pages#photos'
+  get 'about'    => 'pages#about'
+  get 'contact'  => 'pages#contact'
+  get 'business' => 'pages#business'
   
   get 'photos/trx' => 'photos#trx'
 
